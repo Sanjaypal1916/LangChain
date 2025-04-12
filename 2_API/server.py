8c0e2f86dc951c9eef4284406ae405c80f233bbd
Sanjaypal1916 <sanjaypal606060@gmail.com>
2025-04-12 23:58:08 +0530

    api

--- 2_API/server.py
+++ 2_API/server.py
@@ -11,8 +11,27 @@ load_dotenv()
 ## Langmith tracking
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGSMITH_API_KEY")
+
+
 app = FastAPI(
     title="Chatbot API",
     description="A simple chatbot API built with FastAPI and LangChain.",
     version="1.0.0"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "your name is aira and youre an AI code assitant."),
+        ("user", "QUestion":{question})
+    ]
+)
+
+llm = Ollama(model="llama2", tempreture=0.7)
+output = StrOutputParser()
+chain = prompt|llm|output
+
+add_routes(
+    app,
+    chain,
+    path="/ollama/bot"
 )
