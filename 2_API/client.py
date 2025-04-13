dde4bb65f9317d9afbd0a9986c869b4a4543e7e4
Sanjaypal1916 <sanjaypal606060@gmail.com>
2025-04-13 13:31:47 +0530

    successfully run the apis

--- 2_API/client.py
+++ 2_API/client.py
@@ -2,17 +2,31 @@ import streamlit as st
 import requests
 
 def get_response(question):
-    url = "http://localhost:8080/ollama/bot"
-    res = requests.post(url, json={"input" : {"Question": question}})
+    url = "http://localhost:8080/ollama/bot/invoke"
+    res = requests.post(url, json={"input" : {"question": question}})
     print("Status Code:", res.status_code)
     print("Response JSON:", res.json())  # log the actual response
-    return res.json().get('output', "❌ No 'output' key in response")
+    return res.json().get('text', "❌ No 'output' key in response")
 
+def response(question):
+    url = "http://localhost:8080/ollama/bot/invoke"
+    res = requests.post(url, json={"input": {"question": question}})
+    print("Status Code:", res.status_code)
+    response_json = res.json()
+    print("Response JSON:", response_json)  # log the actual response
 
+    try:
+        # Extract the text from the nested generation
+        text_output = response_json["output"]
+    except (KeyError, IndexError) as e:
+        text_output = "❌ Failed to parse generated text from response"
+        print("Error extracting text:", e)
 
-st.title("Chatbot using FASTAPIs")
-question = st.text_input("Enter your question:")
+    return text_output
+
+st.title("The GirlFriend Chatbot")
+question = st.text_input("Enter your feelings here:")
 onoff=  st.button("submit")
 
 if onoff:
-    st.header(get_response(question))
+    st.header(response(question))
--- 2_API/server.py
+++ 2_API/server.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
 from langserve import add_routes
 import uvicorn
 import os
@@ -9,18 +8,18 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_core.output_parsers import StrOutputParser
 
-## Langmith tracking
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGSMITH_API_KEY")
 
 
 app = FastAPI(
-    title="Chatbot API",
+    title="girlfriend chatbot API",
     description="A simple chatbot API built with FastAPI and LangChain.",
     version="1.0.0"
 )
 
-prompt = ChatPromptTemplate.from_messages("user", "QUestion : {question}")
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are Anisa, an AI girlfriend. who is kind and always teaches best for your boyfirend that is the user. talk to him like a girlfriend."),
+    ("user", "Question: {question}")
+])
 
 llm = Ollama(model="llama2")
 output = StrOutputParser()
@@ -34,3 +33,4 @@ add_routes(
 
 if __name__ == "__main__":
     uvicorn.run(app, host= "localhost", port = 8080)
+
